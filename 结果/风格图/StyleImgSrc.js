0e332d65e280533a1a7f9ec056ef3142fb9dace4
finepix <499768985@qq.com>
2018-12-17 12:43:05 +0800

    添加渲染效果

---
+++ 结果/风格图/StyleImgSrc.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yangbingxun on 2017/9/9.
+ */
+chinese.model  cubist.model  egypt.model  feathers.model  girl.model  mosaic.model  scream.model  sea.model  starry.model  udnie.model  versa.model  wave.model
+
+export default {
+    'chinese': 'http://101.132.77.137/resources/styles/chinese.jpg',
+    'cubist': 'http://101.132.77.137/resources/styles/cubist.jpg',
+    'feathers': 'http://101.132.77.137/resources/styles/starry.jpg',
+    'egypt': 'http://101.132.77.137/resources/styles/egypt.jpg',
+    'scream': 'http://101.132.77.137/resources/styles/starry.jpg',
+    'girl': 'http://101.132.77.137/resources/styles/girl.jpg',
+    'mosaic': 'http://101.132.77.137/resources/styles/starry.jpg',
+    'scream': 'http://101.132.77.137/resources/styles/scream.jpg',
+    'sea': 'http://101.132.77.137/resources/styles/sea.jpg',
+    'udnie': 'http://101.132.77.137/resources/styles/udnie.jpg',
+    'versa': 'http://101.132.77.137/resources/styles/versa.jpg',
+    'wave': 'http://101.132.77.137/resources/styles/wave.jpg'
+
+}
